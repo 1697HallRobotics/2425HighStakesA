7870fea700224f1dcce0328eb95ea1bd52b9ead0
Hall Robotics WHPS <hallrobotics.whps@gmail.com>
2024-10-05 10:04:56 -0500

    add redo/undo

--- editor/main.js
+++ editor/main.js
@@ -7,7 +7,54 @@ let stopNextTick = false;
 let isRunning = false;
 let frameCount = 0;
 let playback_cursor = 0;
+let previous_position = 0;
 let fps = 0, fpsInterval = 0, startTime = 0, now = 0, then = 0, elapsed = 0, recordingLength = 0;
+
+function undo() {
+    if (undo_stack.length == 0) return;
+    let undo_action = undo_stack.pop();
+
+    console.log("undo");
+
+    switch (undo_action.action) {
+        case "update_buffer":
+            playback_cursor = undo_action.cursor_position;
+            playback_buffer[playback_cursor][undo_action.common_data[0]][undo_action.common_data[1]] = undo_action.old_data[0]
+            break;
+        case "move_cursor":
+            playback_cursor = undo_action.old_data[0];
+            break;
+        default:
+            break;
+    }
+
+    redo_stack.push(undo_action);
+
+    update_ui(playback_cursor);
+}
+
+function redo() {
+    if (redo_stack.length == 0) return;
+    let redo_action = redo_stack.pop();
+
+    console.log("redo");
+
+    switch (redo_action.action) {
+        case "update_buffer":
+            playback_cursor = redo_action.cursor_position;
+            playback_buffer[playback_cursor][redo_action.common_data[0]][redo_action.common_data[1]] = redo_action.new_data[0]
+            break;
+        case "move_cursor":
+            playback_cursor = redo_action.new_data[0];
+            break;
+        default:
+            break;
+    }
+
+    undo_stack.push(redo_action);
+
+    update_ui(playback_cursor);
+}
 
 function resetBuffer() {
     playback_buffer = structuredClone(original_playback_buffer);
@@ -51,56 +98,67 @@ function save() {
 }
 
 function add_cursor(idx) {
+    undo_stack.push({
+        "action": "move_cursor",
+        "common_data": [],
+        "old_data": [Number(playback_cursor)],
+        "new_data": [Number(playback_cursor) + Number(idx)],
+        "cursor_position": playback_cursor
+    });
     playback_cursor += idx;
     update_ui(playback_cursor);
-    $("#timeline").val(playback_cursor.toString());
 }
 
 function stop_playback() {
-    if (isRunning) {
-        stopNextTick = true;
-    } else {
-        update_ui(0);
-        $("#timeline").val("0");
-    }
+    if (isRunning) stopNextTick = true;
+    else update_ui(0);
+    
 }
 
 function update_ui(idx) {
     $("#progress").val(idx.toString());
+    $("#timeline").val(playback_cursor.toString());
 
     $("#axis1").val(playback_buffer[idx].axis[0]);
     $("#axis2").val(playback_buffer[idx].axis[1]);
     $("#axis3").val(playback_buffer[idx].axis[2]);
     $("#axis4").val(playback_buffer[idx].axis[3]);
 
-    $("#button_a").prop("checked", playback_buffer[idx].digital[0] == 1);
-    $("#button_b").prop("checked", playback_buffer[idx].digital[1] == 1);
-    $("#button_x").prop("checked", playback_buffer[idx].digital[2] == 1);
-    $("#button_y").prop("checked", playback_buffer[idx].digital[3] == 1);
-    $("#button_up").prop("checked", playback_buffer[idx].digital[4] == 1);
+    $("#button_a")    .prop("checked", playback_buffer[idx].digital[0] == 1);
+    $("#button_b")    .prop("checked", playback_buffer[idx].digital[1] == 1);
+    $("#button_x")    .prop("checked", playback_buffer[idx].digital[2] == 1);
+    $("#button_y")    .prop("checked", playback_buffer[idx].digital[3] == 1);
+    $("#button_up")   .prop("checked", playback_buffer[idx].digital[4] == 1);
     $("#button_right").prop("checked", playback_buffer[idx].digital[5] == 1);
-    $("#button_down").prop("checked", playback_buffer[idx].digital[6] == 1);
-    $("#button_left").prop("checked", playback_buffer[idx].digital[7] == 1);
-    $("#button_l1").prop("checked", playback_buffer[idx].digital[8] == 1);
-    $("#button_l2").prop("checked", playback_buffer[idx].digital[9] == 1);
-    $("#button_r1").prop("checked", playback_buffer[idx].digital[10] == 1);
-    $("#button_r2").prop("checked", playback_buffer[idx].digital[11] == 1);
+    $("#button_down") .prop("checked", playback_buffer[idx].digital[6] == 1);
+    $("#button_left") .prop("checked", playback_buffer[idx].digital[7] == 1);
+    $("#button_l1")   .prop("checked", playback_buffer[idx].digital[8] == 1);
+    $("#button_l2")   .prop("checked", playback_buffer[idx].digital[9] == 1);
+    $("#button_r1")   .prop("checked", playback_buffer[idx].digital[10] == 1);
+    $("#button_r2")   .prop("checked", playback_buffer[idx].digital[11] == 1);
 
-    $("#digital_a").toggleClass("pressing", playback_buffer[idx].digital[0] == 1);
-    $("#digital_b").toggleClass("pressing", playback_buffer[idx].digital[1] == 1);
-    $("#digital_x").toggleClass("pressing", playback_buffer[idx].digital[2] == 1);
-    $("#digital_y").toggleClass("pressing", playback_buffer[idx].digital[3] == 1);
-    $("#digital_up").toggleClass("pressing", playback_buffer[idx].digital[4] == 1);
+    $("#digital_a")    .toggleClass("pressing", playback_buffer[idx].digital[0] == 1);
+    $("#digital_b")    .toggleClass("pressing", playback_buffer[idx].digital[1] == 1);
+    $("#digital_x")    .toggleClass("pressing", playback_buffer[idx].digital[2] == 1);
+    $("#digital_y")    .toggleClass("pressing", playback_buffer[idx].digital[3] == 1);
+    $("#digital_up")   .toggleClass("pressing", playback_buffer[idx].digital[4] == 1);
     $("#digital_right").toggleClass("pressing", playback_buffer[idx].digital[5] == 1);
-    $("#digital_down").toggleClass("pressing", playback_buffer[idx].digital[6] == 1);
-    $("#digital_left").toggleClass("pressing", playback_buffer[idx].digital[7] == 1);
-    $("#digital_l1").toggleClass("pressing", playback_buffer[idx].digital[8] == 1);
-    $("#digital_l2").toggleClass("pressing", playback_buffer[idx].digital[9] == 1);
-    $("#digital_r1").toggleClass("pressing", playback_buffer[idx].digital[10] == 1);
-    $("#digital_r2").toggleClass("pressing", playback_buffer[idx].digital[11] == 1);
+    $("#digital_down") .toggleClass("pressing", playback_buffer[idx].digital[6] == 1);
+    $("#digital_left") .toggleClass("pressing", playback_buffer[idx].digital[7] == 1);
+    $("#digital_l1")   .toggleClass("pressing", playback_buffer[idx].digital[8] == 1);
+    $("#digital_l2")   .toggleClass("pressing", playback_buffer[idx].digital[9] == 1);
+    $("#digital_r1")   .toggleClass("pressing", playback_buffer[idx].digital[10] == 1);
+    $("#digital_r2")   .toggleClass("pressing", playback_buffer[idx].digital[11] == 1);
     
     $("#joycon_right").css("transform", "translate(" + (playback_buffer[idx].axis[0] * 2.5) + "px," + (playback_buffer[idx].axis[1] * -2.5) + "px)")
-    $("#joycon_left").css("transform", "translate(" + (playback_buffer[idx].axis[2] * 2.5) + "px," + (playback_buffer[idx].axis[3] * -2.5) + "px)")
+    $("#joycon_left") .css("transform", "translate(" + (playback_buffer[idx].axis[2] * 2.5) + "px," + (playback_buffer[idx].axis[3] * -2.5) + "px)")
+
+    $("#undostack").text(JSON.stringify(undo_stack));
+    $("#redostack").text(JSON.stringify(redo_stack));
+
+    Array.from(document.getElementsByTagName("input")).forEach((item, idx) => {
+        item.blur();
+    })
 }
 
 function begin_playback(start_pos = 0) {
@@ -140,12 +198,20 @@ function tickPlayback() {
         // update stuff
         playback_cursor++;
         update_ui(playback_cursor);
-        $("#timeline").val(playback_cursor.toString());
     }
 }
 
 function saveToBuffer(section, idx, data) {
-    playback_buffer[playback_cursor][section][idx] = data;
+    let newValue = section === "axis" ? data : (data == "on" ? 1 : 0);
+    undo_stack.push({
+        "action": "update_buffer",
+        "common_data": [section, idx],
+        "old_data": [playback_buffer[playback_cursor][section][idx]],
+        "new_data": [newValue],
+        "cursor_position": playback_cursor
+    });
+    playback_buffer[playback_cursor][section][idx] = newValue;
+    
     update_ui(playback_cursor);
 }
 
@@ -174,7 +240,7 @@ window.onload = function () {
             console.log("Data says length is " + length + " seconds.");
             $("#controller_ui").removeClass("hidden");
             $("#data_ui").removeClass("hidden");
-            $("#timeline").css("width", (length * 100) + "px");
+            $("#timeline").css("width", (length * 40) + "px");
             $("#timeline").attr("max", playback_buffer.length - 1);
             $("#progress").val("0");
             $("#max").text("/" + (playback_buffer.length - 1).toString());
@@ -186,27 +252,44 @@ window.onload = function () {
         reader.readAsArrayBuffer(input.files[0]);
     });
 
-    document.getElementById("axis1")        .addEventListener("change", e => { saveToBuffer("axis", 0, e.target.value) });
-    document.getElementById("axis2")        .addEventListener("change", e => { saveToBuffer("axis", 1, e.target.value) });
-    document.getElementById("axis3")        .addEventListener("change", e => { saveToBuffer("axis", 2, e.target.value) });
-    document.getElementById("axis4")        .addEventListener("change", e => { saveToBuffer("axis", 3, e.target.value) });
-    document.getElementById("button_a")     .addEventListener("change", e => { saveToBuffer("digital", 0, e.target.value) });
-    document.getElementById("button_b")     .addEventListener("change", e => { saveToBuffer("digital", 1, e.target.value) });
-    document.getElementById("button_x")     .addEventListener("change", e => { saveToBuffer("digital", 2, e.target.value) });
-    document.getElementById("button_y")     .addEventListener("change", e => { saveToBuffer("digital", 3, e.target.value) });
-    document.getElementById("button_up")    .addEventListener("change", e => { saveToBuffer("digital", 4, e.target.value) });
-    document.getElementById("button_right") .addEventListener("change", e => { saveToBuffer("digital", 5, e.target.value) });
-    document.getElementById("button_down")  .addEventListener("change", e => { saveToBuffer("digital", 6, e.target.value) });
-    document.getElementById("button_left")  .addEventListener("change", e => { saveToBuffer("digital", 7, e.target.value) });
-    document.getElementById("button_l1")    .addEventListener("change", e => { saveToBuffer("digital", 8, e.target.value) });
-    document.getElementById("button_l2")    .addEventListener("change", e => { saveToBuffer("digital", 9, e.target.value) });
-    document.getElementById("button_r1")    .addEventListener("change", e => { saveToBuffer("digital", 10, e.target.value) });
-    document.getElementById("button_r2")    .addEventListener("change", e => { saveToBuffer("digital", 11, e.target.value) });
+    document.getElementById("axis1")        .addEventListener("change", e => { saveToBuffer("axis",     0, e.target.value); e.target.blur() });
+    document.getElementById("axis2")        .addEventListener("change", e => { saveToBuffer("axis",     1, e.target.value); e.target.blur() });
+    document.getElementById("axis3")        .addEventListener("change", e => { saveToBuffer("axis",     2, e.target.value); e.target.blur() });
+    document.getElementById("axis4")        .addEventListener("change", e => { saveToBuffer("axis",     3, e.target.value); e.target.blur() });
+    document.getElementById("button_a")     .addEventListener("change", e => { saveToBuffer("digital",  0, e.target.value); e.target.blur() });
+    document.getElementById("button_b")     .addEventListener("change", e => { saveToBuffer("digital",  1, e.target.value); e.target.blur() });
+    document.getElementById("button_x")     .addEventListener("change", e => { saveToBuffer("digital",  2, e.target.value); e.target.blur() });
+    document.getElementById("button_y")     .addEventListener("change", e => { saveToBuffer("digital",  3, e.target.value); e.target.blur() });
+    document.getElementById("button_up")    .addEventListener("change", e => { saveToBuffer("digital",  4, e.target.value); e.target.blur() });
+    document.getElementById("button_right") .addEventListener("change", e => { saveToBuffer("digital",  5, e.target.value); e.target.blur() });
+    document.getElementById("button_down")  .addEventListener("change", e => { saveToBuffer("digital",  6, e.target.value); e.target.blur() });
+    document.getElementById("button_left")  .addEventListener("change", e => { saveToBuffer("digital",  7, e.target.value); e.target.blur() });
+    document.getElementById("button_l1")    .addEventListener("change", e => { saveToBuffer("digital",  8, e.target.value); e.target.blur() });
+    document.getElementById("button_l2")    .addEventListener("change", e => { saveToBuffer("digital",  9, e.target.value); e.target.blur() });
+    document.getElementById("button_r1")    .addEventListener("change", e => { saveToBuffer("digital", 10, e.target.value); e.target.blur() });
+    document.getElementById("button_r2")    .addEventListener("change", e => { saveToBuffer("digital", 11, e.target.value); e.target.blur() });
+    
+    document.getElementById("timeline")     .addEventListener("mouseup", e => {
+        e.preventDefault();
+        undo_stack.push({
+            "action": "move_cursor",
+            "common_data": [],
+            "old_data": [Number(previous_position)],
+            "new_data": [Number(playback_cursor)],
+            "cursor_position": playback_cursor
+        })
+        previous_position = playback_cursor;
+    })
 }
 
-window.onkeydown = function (ev) {
-    if (ev.ctrlKey && ev.key == 'z') {
-        if (ev.shiftKey) {} // redo
-        //TODO: implement undo/redo
+window.onkeydown = function (evt) {
+    if ((evt.key === 'Z' && (evt.ctrlKey || evt.metaKey) && evt.shiftKey) || (evt.key === 'y' && (evt.ctrlKey || evt.metaKey))) {
+        evt.preventDefault();
+        // handle redo action
+        redo();
+    } else if (evt.key === 'z' && (evt.ctrlKey || evt.metaKey)) {
+        evt.preventDefault();
+        // handle undo action
+        undo();
     }
 }
